fcb34944a645a443c8c2228a89077f41cb06800a
mstbeat <mrbrightside82@ymail.com>
2020-09-26 20:51:34 +0900

    finish ch93 getter/setter

---
+++ 060_関数とオブジェクト２/150_プロパティーとディスクリプター/start/main.js
@@ -0,0 +1,23 @@
+'use strict';
+// const obj = {prop: 0};
+
+const obj = {};
+
+Object.defineProperty(obj, 'prop', {
+  value: 0,
+  writable: true,
+  configurable: true
+})
+
+// Object.defineProperty(obj, 'prop', {
+//   enumerable: true
+// })
+
+delete obj.prop;
+
+// obj.prop = 1;
+console.log(obj.prop);
+
+const descriptor = Object.getOwnPropertyDescriptor(obj, 'prop');
+
+console.log(descriptor);
--- 060_関数とオブジェクト２/160_getter、setterとstatic/start/main.js
+++ 060_関数とオブジェクト２/160_getter、setterとstatic/start/main.js
@@ -1,4 +1,50 @@
 function Person1(name, age) {
-	this.name = name;
-	this.age = age;
+	this._name = name;
+	this._age = age;
 }
+
+Person1.hello = function() {
+	console.log('hello');
+}
+Person1.hello();
+
+Object.defineProperty(Person1.prototype, 'name', {
+	get: function() {
+		return this._name;
+	},
+	set: function(val) {
+		this._name = val;
+	}
+})
+
+const p1 = new Person1('Bob', 23);
+console.log(p1.name);
+
+p1.name = 'Tom';
+console.log(p1.name);
+
+class Person2 {
+	constructor(name, age) {
+		this._name = name;
+		this._age = age;
+	}
+
+	get name() {
+		return this._name;
+	}
+
+	set name(val) {
+		this._name = val;
+	}
+
+	static hello() {
+		console.log('hello');
+	}
+}
+
+const p2 = new Person2('Billy', 20);
+console.log(p2.name);
+p2.name = 'Martin';
+console.log(p2.name);
+
+Person2.hello();
