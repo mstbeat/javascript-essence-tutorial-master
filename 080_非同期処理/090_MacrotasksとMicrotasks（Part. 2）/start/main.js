06e831d2b795e6d52ab0bf497d288065d58bdb3e
mstbeat <mrbrightside82@ymail.com>
2020-09-29 16:54:30 +0900

    finish sec10 async function

--- 080_非同期処理/090_MacrotasksとMicrotasks（Part. 2）/start/main.js
+++ 080_非同期処理/090_MacrotasksとMicrotasks（Part. 2）/start/main.js
@@ -6,8 +6,25 @@ new Promise(function promise(resolve) {
     resolve();
   });
 
+  setTimeout(function task2() {
+    console.log('task2');
+
+    const p = Promise.resolve();
+    p.then(function job4(){
+      console.log('job4');
+    })
+    queueMicrotask(function job4() {
+      console.log('job4');
+    })
+  });
+
 }).then(function job1() {
   console.log('job1');
+
+}).then(function job2() {
+  console.log('job2');
+}).then(function job3() {
+  console.log('job3');
 })
 
 console.log('global end');
